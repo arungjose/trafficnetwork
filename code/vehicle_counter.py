aed28c8069cb9ea37b5b5ef3dfe34945e91478c6
arungjose <arunjose2004@gmail.com>
2025-02-16 16:20:41 +0530

    Update code and added random count generator

--- code/vehicle_counter.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO(r'C:\Users\ARUN\Desktop\ATN\yolo11l.pt')
-
-desired_classes = ['motorcycle', 'bus', 'car', 'truck', 'bicycle']
-results = model(r'C:\Users\ARUN\Desktop\ATN\dataset\traffic_stock (2).mp4')  
-
-
-print("HHEELLOO", result, "HHEELLOO")
