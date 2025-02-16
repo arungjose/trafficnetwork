17bf87b9f54f45d2430dcbda7dc4de45b17e5157
arungjose <arunjose2004@gmail.com>
2025-02-16 22:44:11 +0530

    Added the server program

---
+++ code/server.py
@@ -0,0 +1,19 @@
+# Server code --> Node A
+from socket import *
+from time import *
+filename= 'lane1.txt'
+def readfile(filename):
+    with open(filename, 'r') as file:
+        dict_str = file.read()
+        return(dict_str)
+server = socket(AF_INET, SOCK_STREAM)
+server.bind(('localhost', 12000))
+server.listen()
+print('Server listening at 12000')
+connection, address = server.accept()
+print('Connected to client')
+data =readfile(filename)
+connection.send(bytes(data + ctime(), 'utf-8'))
+recData = connection.recv(1024).decode()
+print('Client:', recData)
+connection.close()
