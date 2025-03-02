6f2449191ab1e20a6336db26c1e0af96d62d2741
arungjose <arunjose2004@gmail.com>
2025-03-02 13:49:15 +0530

    Creates vehicle count for adjacent junctions

--- code/random_data.py
+++ code/random_data.py
@@ -1,9 +1,15 @@
 import random
 
 # File pointers to write into file
-f1 =  open("code/lane1.txt", "w")
-f2 =  open("code/lane2.txt", "w")
-f3 =  open("code/lane3.txt", "w")
+f1 =  open("code/lane2.txt", "w")
+f2 =  open("code/lane3.txt", "w")
+f3 =  open("code/lane4.txt", "w")
+
+# Adjacent juntion lanes
+r1 =  open("code/t_lane1.txt", "w")
+r2 =  open("code/t_lane2.txt", "w")
+r3 =  open("code/t_lane3.txt", "w")
+r4 =  open("code/t_lane4.txt", "w")
 
 # Vehicle classs to choose from 
 vehicles = ['bicycle', 'car', 'motorcycle', 'bus', 'truck']
@@ -25,7 +31,17 @@ f1.write(str(random_count()))
 f2.write(str(random_count()))
 f3.write(str(random_count()))
 
+# Writing random counts to files --> t_lan1, t_lane2, t_lane3, t_lane4 [Adjacent Junction]
+r1.write(str(random_count()))
+r2.write(str(random_count()))
+r3.write(str(random_count()))
+r4.write(str(random_count()))
+
 # Clearing all buffers and releasing all resources
 f1.close()
 f2.close()
 f3.close()
+r1.close()
+r2.close()
+r3.close()
+r4.close()
